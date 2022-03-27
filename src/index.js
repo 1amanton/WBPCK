d2b2e82706d9edd13b9e5bfbbaf77b712787a3b6
1amanton <iamantongit@gmail.com>
2022-03-27 13:26:19 +0300

    add resolve extensions default file formats

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Post from "./Post"
 import "./styles/style.css"
 import jsonfile from "./assets/jsonfile"
-import foxImage from "./assets/fox.png"
+import foxImage from "./assets/fox"
 
 const post = new Post(`Webpack Post Title`, foxImage)
 console.log(`Post to String: ${post.toString()}`)
