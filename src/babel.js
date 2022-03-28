eb9a05f834f390c1e3d464223f191665982afb4a
1amanton <iamantongit@gmail.com>
2022-03-28 14:53:58 +0300

    add BABEL proposalClassProperties plugin

--- src/babel.js
+++ src/babel.js
@@ -2,3 +2,9 @@ async function start() {
     return await Promise.resolve("async is working")
 }
 start().then(console.log)
+
+class Util {
+    static id = Date.now()
+}
+
+console.log("Util Id:", Util.id)
